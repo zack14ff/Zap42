cd50e7900b8bcbedead1a186d94b76d8381d45dd
zack14ff <97785456+zack14ff@users.noreply.github.com>
2025-02-23 18:38:46 +0300

    Update ZAP14FF.lua
    Fully Released V5.5.5.5

--- Scripty/ZAP14FF.lua
+++ Scripty/ZAP14FF.lua
@@ -1,1 +1,46 @@
 -- these must be Zap12| mini
+
+
+-- Library
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+-- Name
+local Window = Library.CreateLib("Zap14ff|Mover", "DarkTheme")
+-- Tabs
+local Tab = Window:NewTab("Keyless scripts")
+local TabP = Window:NewTab("Key System scripts")
+-- KeyLESS argument
+local Section = Tab:NewSection("Zapped")
+
+Section:NewButton("Zap42(no work)", "FREE FREE FREEEEEEEEEEEEEEE", function()
+    print("Clicked")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/zack14ff/Zap42/refs/heads/main/Scripty/ZAP42.lua'))()
+end)
+
+Section:NewButton("FarsakenPirated", "ONLY WORK ON FORSAKEN", function()
+    print("Last update 22.02.2025")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/zack14ff/Zap42/refs/heads/main/Scripty/FartSakenedPiracy.lua'))()
+end)
+
+-- key request
+
+local Section = TabP:NewSection("ERM KEY!")
+
+Section:NewButton("Pipidastr - Unlimited(no work)", "KEYSYSTEM", function()
+    print("PAYMENT!!!")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zack14ff/Zap42/refs/heads/main/Scripty/PipiDastr - Unlimited.lua'))()
+end)
+
+Section:NewButton("AlienSaken(no work)", "WORK ONLY ON FORSAKEN", function()
+    print("Clicked")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/zack14ff/Zap42/refs/heads/main/Scripty/AlienSaken.lua'))()
+end)
+
+Section:NewButton("Elien Hub | Forsaken", "WORK ONLY ON FORSAKEN", function()
+    print("Clicked")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Selivus07/forsakenfr/refs/heads/main/ElysianV.lua'))()
+end)
+
+Section:NewButton("FartSaken", "MOST COOL CHEAT ON FORSAKEN BUT AIM NO WORK ON DUSEKKAR AND COOLKID", function()
+    print("Clicked")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/ivannetta/ShitScripts/refs/heads/main/forsaken.lua'))()
+end)
